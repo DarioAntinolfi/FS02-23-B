980fca5daafc58562bb40f2cb9ca828710e6007f
DarioAntinolfi <123833938+DarioAntinolfi@users.noreply.github.com>
2023-06-12 14:28:38 +0200

    .

--- Lezioni/Settimana12/Lezione/ripasso/src/app/app.module.ts
+++ Lezioni/Settimana12/Lezione/ripasso/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UtentiComponent } from './components/utenti/utenti.component';
+
+const rotte: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'utenti',
+    component: UtentiComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    UtentiComponent
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot(rotte)
   ],
   providers: [],
   bootstrap: [AppComponent]
